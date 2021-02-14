289ac5fe65bbead11d43ad5fa250b26a639e2b4a
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-02-14 22:36:27 +0100

    recipes endpoint is working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-/**
- * This says hi
- * @param {number} int
- * @param {string}
- */
-function hi(int) {
-  console.log("Hello World!");
-  return "hi";
-}
+const express = require("express");
+const recommend = require("./controllers/recommender");
+
+const app = express();
+app.get("/test", (req, res) => {
+  res.send(`${Date.now()}`);
+});
+app.get("/hi", (req, res) => {
+  res.send(`hi`);
+});
+app.get("/recipes", function (req, res, next) {
+  recommend
+    .get_recipes()
+    .then((recipe) => {
+      res.send(recipe);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.listen(5000);
+module.exports = app; // for testing
