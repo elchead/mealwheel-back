1accc71dc3a34bf3004bae1480fdf060c5b7430a
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-02-15 08:54:51 +0100

    bugfix in file existent

--- src/utils/file.js
+++ src/utils/file.js
@@ -7,8 +7,40 @@ class FileError extends Error {
 
 const isFileExistent = (filepath) => {
   const fs = require("fs");
-  if (!fs.existsSync(filename)) {
-    throw new FileError(filename);
+  if (!fs.existsSync(filepath)) {
+    throw new FileError(filepath);
   }
 };
-module.exports = { FileError, isFileExistent };
+
+const spawn = require("child_process").spawn;
+// not working
+class PythonExecutor {
+  constructor(filepath) {
+    this.path = filepath;
+    this.process = spawn("python", [filepath]);
+  }
+  getOutput() {
+    try {
+      console.log(isFileExistent(this.path));
+    } catch (err) {
+      console.error(err);
+    }
+    var res = "";
+    const processo = spawn("python", [this.path]);
+    processo.stdout.on("data", (data) => {
+      data = data.toString();
+      res += data;
+    });
+    console.log(res);
+    // console.log(res);
+    // pythonProcess.on("close", (code) => {
+    //   resolve(res);
+    // });
+    // pythonProcess.on("error", (err) => {
+    //   console.error(err);
+    //   reject(err);
+    // });
+    return res;
+  }
+}
+module.exports = { FileError, PythonExecutor, isFileExistent };
