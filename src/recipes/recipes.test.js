279e21246f4c3a68981ea5ef5cc2ef059c9fd12c
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-03-07 12:53:43 +0100

    fix test of recipes without auth

--- src/recipes/recipes.test.js
+++ src/recipes/recipes.test.js
@@ -3,25 +3,20 @@ const { expect } = require("chai");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const server = require("../index");
+const recommend = require("./recipes.service");
 // Configure chai
 chai.use(chaiHttp);
 chai.should();
 
 describe("recipes", () => {
-  it("should fetch", (done) => {
-    chai
-      .request(server)
-      .get("/recipes")
-      .end((err, res) => {
-        res.should.have.status(200);
-        console.log(res.body.Ingredients);
-        expect(res).to.be.json;
-        const expected = {
-          Ingredients: ["Apple", "Banana"],
-          Steps: "Do that...",
-        };
-        res.body.should.be.eql(expected);
-        done();
-      });
+  it("should fetch", async () => {
+    // chai;
+    const res = await recommend.getRecipes().catch(console.error);
+    const recipe = JSON.parse(res);
+    const expected = {
+      Ingredients: ["Apple", "Banana"],
+      Steps: "Do that...",
+    };
+    recipe.should.be.eql(expected);
   });
 });
